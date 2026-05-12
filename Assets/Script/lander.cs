647d9537d45e60aa82518621aed7476179d0445b
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-05-12 08:24:16 +0545

    rocket lander smooth and sorting orders

--- Assets/Script/lander.cs
+++ Assets/Script/lander.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class lander : MonoBehaviour
-{
-    
-    private void Update()
-    {
-      
+public class lander : MonoBehaviour{
+    private Rigidbody2D landerRigidbody2D;
+
+    private void Awake(){
+
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate(){
+
+
+        if (Keyboard.current.upArrowKey.isPressed){
+            float force = 700f;
+            landerRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
+        }
+        if (Keyboard.current.leftArrowKey.isPressed){
+            float turnSpeed = +100f;
+            landerRigidbody2D.AddTorque(+turnSpeed * Time.deltaTime);
+        }
+        if (Keyboard.current.rightArrowKey.isPressed){
+            float turnSpeed = -100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("oncollision 2d enter");
     }
 }
